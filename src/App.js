c85dd04243fd4ad67ecc8367790e9808e3ea680a
Tommy Wong <000518wong@gmail.com>
2021-01-23 03:54:32 +0800

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
 import './App.css';
+import { drawHand } from './utilities';
+
+import { useRef } from 'react';
+import * as tf from '@tensorflow/tfjs';
+import * as handpose from '@tensorflow-models/handpose';
+import Webcam from 'react-webcam';
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  const runHandPose = async () => {
+    // load tensorflow handpose neural network
+    const net = await handpose.load();
+    console.log("Handpose loaded.");
+
+    // create loop to detect hand neural network
+    setInterval(() => {
+      detect(net);
+    }, 100)
+  }
+
+  const detect = async (net) => {
+    // check data is available
+    if (
+      webcamRef.current &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // get video properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // set video height and width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // set canvas height and width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      // make detections
+      const hand = await net.estimateHands(video);
+      console.log(hand);
+
+      // draw mesh
+      const ctx = canvasRef.current.getContext("2d");
+      drawHand(hand, ctx);
+    }
+  }
+  
+
+  runHandPose();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Webcam ref={webcamRef} style={webcamStyle} />
+        <canvas id="myCanvas" ref={canvasRef} style={canvasStyle} />
       </header>
     </div>
   );
+}
+
+const webcamStyle = {
+  position: 'absolute',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  left: 0,
+  right: 0,
+  textAlign: 'center',
+  width: 640,
+  height: 480,
+  zindex: 9,
+}
+
+const canvasStyle = {
+  position: 'absolute',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  left: 0,
+  right: 0,
+  textAlign: 'center',
+  width: 640,
+  height: 480,
+  zindex: 9,
 }
 
 export default App;
---
+++ src/utilities.js
@@ -0,0 +1,81 @@
+// finger joins dictionary
+const fingerJoints = {
+  thumb: [0, 1, 2, 3, 4],
+  indexFinger: [0, 5, 6, 7, 8],
+  middleFinger: [0, 9, 10, 11, 12],
+  ringFinger: [0, 13, 14, 15, 16],
+  pinky: [0, 17, 18, 19, 20]
+}
+
+// Infinity Gauntlet Style
+const style = {
+  0: { color: "yellow", size: 15 },
+  1: { color: "gold", size: 6 },
+  2: { color: "green", size: 10 },
+  3: { color: "gold", size: 6 },
+  4: { color: "gold", size: 6 },
+  5: { color: "purple", size: 10 },
+  6: { color: "gold", size: 6 },
+  7: { color: "gold", size: 6 },
+  8: { color: "gold", size: 6 },
+  9: { color: "blue", size: 10 },
+  10: { color: "gold", size: 6 },
+  11: { color: "gold", size: 6 },
+  12: { color: "gold", size: 6 },
+  13: { color: "red", size: 10 },
+  14: { color: "gold", size: 6 },
+  15: { color: "gold", size: 6 },
+  16: { color: "gold", size: 6 },
+  17: { color: "orange", size: 10 },
+  18: { color: "gold", size: 6 },
+  19: { color: "gold", size: 6 },
+  20: { color: "gold", size: 6 },
+};
+
+export const drawHand = (predictions, ctx) => {
+
+  if (predictions.length > 0) {
+    // for all prediction, actually just one prediction
+    predictions.forEach(prediction => {
+      const landmarks = prediction.landmarks;
+
+      // loop thru the available landmarks
+      for (let i = 0; i < landmarks.length; i++) {
+        let x = landmarks[i][0];
+        let y = landmarks[i][1];
+
+        // start drawing
+        ctx.beginPath();
+        ctx.arc(x, y, style[i].size, 0, 2*Math.PI);
+
+        ctx.fillStyle = style[i].color;
+        ctx.fill();
+      }
+
+      // loop through fingers
+      for (let j = 0; j < Object.keys(fingerJoints).length; j++) {
+        let finger = Object.keys(fingerJoints)[j];
+        //  loop through pairs of joints
+        for (let k = 0; k < fingerJoints[finger].length - 1; k++) {
+          // Get pairs of joints
+          const firstJointIndex = fingerJoints[finger][k];
+          const secondJointIndex = fingerJoints[finger][k + 1];
+
+          // Draw path
+          ctx.beginPath();
+          ctx.moveTo(
+            landmarks[firstJointIndex][0],
+            landmarks[firstJointIndex][1]
+          );
+          ctx.lineTo(
+            landmarks[secondJointIndex][0],
+            landmarks[secondJointIndex][1]
+          );
+          ctx.strokeStyle = "gold";
+          ctx.lineWidth = 6;
+          ctx.stroke();
+        }
+      }
+    })
+  }
+}
